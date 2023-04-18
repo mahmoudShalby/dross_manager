3be2d447ed922826091ebaa9d4feda9c6cbc09b1
Mahmoud Shalaby <mhmooud074@gmail.com>
2023-04-19 00:38:51 +0200

    Finished design(really!)

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.html'],
+  theme: {
+    extend: {
+      colors: {
+        "header": "#456789",
+        "field": "#44668880",
+      }
+    },
+  },
+  plugins: [],
+}
