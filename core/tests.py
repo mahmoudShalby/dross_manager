28acb1a59912e474f4a19a0175f9515575997ac1
Mahmoud Shalaby <mhmooud074@gmail.com>
2023-04-19 19:46:41 +0200

    Tried testing for first time

--- core/tests.py
+++ core/tests.py
@@ -3,13 +3,16 @@ from .models import Stage, Grade, Group, Test, Student
 from django.contrib.auth.models import User
 from random import randint
 
-class StageTestCase(TestCase):
-  def setup(self):
-    owner = User.objects.get(username='admin')
-    stage = Stage.objects.create(owner=owner, from_age=7, name='prim')
-    grades = (Grade.objects.create(stage=stage, index_in_stage=index+1) for index in range(6))
-    groups = (Group.objects.create(grade=grades[index], classes=24, class_price=10) for index in range(6))
-    students = []
-    for grade_index in range(6):
-      for _ in range(10):
-        students.append(Student.objects.create(group=groups[grade_index], name=(('Ahmed', 'Mohamed', 'Mahmoud')[randint(0, 2)])))
+class CoreTestCase(TestCase):
+  def main(self):
+    owner = User.objects.create(username='ahmed')
+    stage = Stage.objects.create(name='primary', owner=owner)
+    grade = Grade.objects.create(stage=stage)
+    group = Group.objects.create(grade=grade, classes=24, class_price=10)
+    [Student.objects.create(group=group, name=name) for name in ('Ahmed', 'Mohamed', 'Mahmoud')]
+
+    owner = User.objects.get(username='ahmed')
+    stage = Stage.objects.get(owner=owner, name='primary')
+    grade = Grade.objects.get(stage=stage)
+    group = Group.objects.get(grade=grade)
+    self.assertEqual(Student.objects.get(group=group, name='Mohamed').name, 'Mohamed')
