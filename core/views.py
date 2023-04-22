725a569f82aa52c3ac78085af96732568dfc5dec
Mahmoud Shalaby <mhmooud074@gmail.com>
2023-04-22 12:11:49 +0200

    Upgraded login design

--- core/views.py
+++ core/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth import authenticate, login
+from django.http import JsonResponse
 
-# Create your views here.
+def login_view(request):
+  if request.method == 'POST':
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+    if not all((username, password)):
+      return JsonResponse({'exists': False})
+
+    user = authenticate(request)
+    if not user:
+      return JsonResponse({'wrong': False})
+
+    login(user)
+    return redirect('/')
+
+  return render(request, 'core/login.html', {})
